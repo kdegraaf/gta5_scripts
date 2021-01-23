709d792543534b23bb7a1ab4c2e6c729a1c111f9
Kevin DeGraaf <kevin@kdegraaf.net>
2021-01-22 21:02:03 -0500

    Tweak solo-session.ps1

--- solo-session.ps1
+++ solo-session.ps1
@@ -1,4 +1,5 @@
 $process = Get-Process gta5 -ErrorAction SilentlyContinue
+
 if ($process) {
   & 'C:\Program Files\PSTools\pssuspend' gta5.exe
   for ($i=10; $i -gt 0; $i--) {
@@ -8,9 +9,11 @@ if ($process) {
   Write-Host
   & 'C:\Program Files\PSTools\pssuspend' -r gta5.exe
   Write-Host "Hopefully, that gave you a clean public session.  Have fun!"
-  Start-Sleep -Seconds 2
 }
+
 else {
   Write-Host "Cannot find process gta5.exe, sorry."
 }
+
 Remove-Variable process
+Start-Sleep -Seconds 2
